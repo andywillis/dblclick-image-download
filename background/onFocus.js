4e2cfc3bbbd83c9e459ba4cdffffeb016764172f
Andy Willis <github@awillis.fastmail.fm>
2017-09-23 04:08:11 +0100

    Fixed onUpdate error

--- background/onFocus.js
+++ background/onFocus.js
@@ -8,11 +8,22 @@
     console.log(`Error: ${error}`);
   }
 
-  browser.tabs.onActivated.addListener(function ({ tabId }) {
+  function loader(tabId) {
     const file = './content/dblclick-image-download.js';
     browser.tabs.executeScript(tabId, { file })
       .then(function (value) { onExecuted({ value, tabId }); })
       .catch(onError);
-  });
+  }
+
+  function handleActivated({ tabId }) {
+    loader(tabId);
+  }
+
+  function handleUpdated(tabId) {
+    loader(tabId);
+  }
+
+  browser.tabs.onUpdated.addListener(handleUpdated);
+  browser.tabs.onActivated.addListener(handleActivated);
 
 }());
